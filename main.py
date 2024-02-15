8ba62c2e6ef2c9c4bb09426a73ad1d1c8798100a
Jackson Carter <jackson.carter@students.fhu.edu>
2024-02-15 15:40:40 -0600

    redo
    fixing it?

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ class Queue():
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def push(self, card):
-        self.cards.append(0)
+        self.cards.append(card)
 
     #REMOVE PASS AND COMPLETE THE FUNCTION
     def pop(self):
         if self.cards:
             return self.cards.pop(0)
+        else:
+            return None
         
 
 if __name__ == '__main__':
     #REMOVE PASS AND YOUR CODE GOES HERE
     my_queue = Queue()
+    my_queue.push("Card 1")
+    my_queue.push("Card 2")
+    my_queue.push("Card 3")
+    print(my_queue.pop())
+    print(my_queue.pop())
+    print(my_queue.pop())
